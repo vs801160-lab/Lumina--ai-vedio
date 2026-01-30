684529f2b46936aefae45a43fc125abc492c17be
vs801160-lab <vs801160@gmail.com>
2026-01-30 23:38:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'shimmer': 'shimmer 2s linear infinite',
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
