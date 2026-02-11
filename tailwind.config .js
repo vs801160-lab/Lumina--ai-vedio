d421b61e2c8230fa1781511c05b0d37532d9a51f
vs801160-lab <vs801160@gmail.com>
2026-02-11 20:59:47 +0530

    Update tailwind.config .js

--- tailwind.config .js
+++ tailwind.config .js
@@ -1,25 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./index.tsx",
-    "./App.tsx",
+    "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      animation: {
-        'shimmer': 'shimmer 2s linear infinite',
-      },
-      keyframes: {
-        shimmer: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(100%)' },
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
